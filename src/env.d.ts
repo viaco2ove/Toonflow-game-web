7d95e91f355c82425ebc01449be078c9b0f0c9c7
viaco2ove <viaco2ove@users.noreply.github.com>
2026-03-24 03:38:25 +0800

    refactor: restructure app and remove deprecated files

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,5 @@
-/// <reference types="vite/client" />
-
 declare module "*.vue" {
   import type { DefineComponent } from "vue";
-  const component: DefineComponent<Record<string, never>, Record<string, never>, unknown>;
+  const component: DefineComponent<Record<string, unknown>, Record<string, unknown>, unknown>;
   export default component;
 }
