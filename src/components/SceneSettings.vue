1718549261a53d1d555a25b505de5d4c80f4703c
viaco2ove <viaco2ove@users.noreply.github.com>
2026-09-16 05:43:57 +0800

    feat(scene): 优化请求地址配置与版本信息显示
    - 在请求地址配置区新增应用和网页版本号显示
    - 调整其他区域中版本号显示样式，保持一致
    - 保留token消耗及检查更新按钮功能
    - 添加新静态资源文件index-DlZ0rOK3.css和index-mL95mRAM.js

--- src/components/SceneSettings.vue
+++ src/components/SceneSettings.vue
@@ -289,7 +289,7 @@ watch(
         <button class="button primary settings-solid-btn" type="button" @click="store.saveConnection">保存连接</button>
       </div>
       <div class="settings-action-row">
-        <span>App:<span v-if="appVersion" class="settings-version-row"> v{{ appVersion }}</span></span>
+        <span>App:<span v-if="appVersion" class="settings-version-row"> v{{ appVersion }}</span></span> |
          <span>Web:<span v-if="webVersion" class="settings-version-row"> v{{ webVersion }}</span></span>
       </div>
     </section>
@@ -508,10 +508,16 @@ watch(
     <section class="surface section-block settings-card settings-card--plain">
       <div class="section-title settings-section-title">其他</div>
       <div class="settings-action-row">
-        <span>App:<span v-if="appVersion" class="settings-version-row"> v{{ appVersion }}</span></span>
-         <span>Web:<span v-if="webVersion" class="settings-version-row"> v{{ webVersion }}</span></span>
-        <button v-if="store.state.token" class="button settings-outline-btn" type="button" @click="openTokenUsageDialog">token消耗</button>
-        <button class="button settings-outline-btn" type="button" @click="checkUpdate">检查更新</button>
+        <span>App:<span v-if="appVersion" class="settings-version-row"> v{{ appVersion }}</span></span> |
+        <span>Web:<span v-if="webVersion" class="settings-version-row"> v{{ webVersion }}</span></span>
+        <div>
+          <button v-if="store.state.token" class="button settings-outline-btn" type="button"
+                  @click="openTokenUsageDialog">token消耗
+          </button>
+          <button class="button settings-outline-btn" type="button" @click="checkUpdate">检查更新</button>
+
+        </div>
+
       </div>
     </section>
   </main>
